4b185cfcdc61e28096412e553857b422b06d2f5c
MarvinJWendt <git@marvinjwendt.com>
2020-08-13 05:44:13 +0200

    feat: create action

---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+
+echo "## Initializing git repo..."
+git init
+echo "### Adding git remote..."
+git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+echo "### Getting branch"
+BRANCH=${GITHUB_REF#*refs/heads/}
+echo "### git fetch $BRANCH ..."
+git fetch origin $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+git checkout $BRANCH
+
+echo "## Login into git..."
+git config --global user.email "git@marvinjwendt.com"
+git config --global user.name "MarvinJWendt"
+
+echo "## Ignore workflow files (we may not touch them)"
+git update-index --assume-unchanged .github/workflows/*
+
+# Start release
+
+git fetch --tags
+
+echo "## Detecting current version of dops"
+OLD_VERSION=$(git tag | tail -1)
+echo "## $OLD_VERSION"
+
+NEW_VERSION=$(cat dops.go | grep "<---VERSION--->" | grep -oP "v\d*\.\d*\.\d*")
+
+echo "## Version in commit detected: $NEW_VERSION!"
+
+if [ "$OLD_VERSION" = "$NEW_VERSION" ]; then
+  echo "## Version did not change. Aborting!"
+else
+  echo "## Version change detected!"
+
+  git tag "$NEW_VERSION"
+
+  echo "## Staging changes..."
+  git add .
+  echo "## Commiting files..."
+  git commit -m "ci: release new version" || true
+  echo "## Pushing to $BRANCH"
+  git push -u origin $BRANCH --tags
+fi
